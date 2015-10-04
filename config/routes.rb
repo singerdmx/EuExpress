8a9f9ed3abba59a682f75bb40358633fcbd519ea
Xin Yao <singerdmx@gmail.com>
2015-10-03 18:07:34 -0700

    Add static pages as home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
+  root to: 'static_pages#home'
   mount Forem::Engine, :at => '/forums'
 
   devise_for :users
