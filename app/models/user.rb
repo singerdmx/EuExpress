d7eb5dd75ea7a3ca30cef8fbdb219f3fcff210d1
Xin Yao <singerdmx@gmail.com>
2015-10-26 07:14:46 -0700

    Add edit user page to add name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :timeoutable
 
   class << self
     def moderate_first_post
