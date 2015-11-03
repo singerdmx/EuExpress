9bc2779df300902b0db158dea0559f7904238a28
Xin Yao <singerdmx@gmail.com>
2015-11-03 08:02:04 -0800

    Remove params topicId and postId in function openModal

--- app/assets/javascripts/forum.js
+++ app/assets/javascripts/forum.js
@@ -263,7 +263,7 @@
             };
             $log.info('Topics table definition', tableDefinition);
             $('table#topicsTable').dataTable(tableDefinition);
-            var refreshButtonHtml = '<button ng-click="openModal(\'New Topic\', \'\', \'\', \'\', \'\')" class="btn btn-danger" type="button"><i class="glyphicon glyphicon-pencil"></i>&nbsp;New Topic</button>' +
+            var refreshButtonHtml = '<button ng-click="openModal(\'New Topic\', \'\', \'\')" class="btn btn-danger" type="button"><i class="glyphicon glyphicon-pencil"></i>&nbsp;New Topic</button>' +
                 '<button class="btn btn-info" type="button" ng-click="refreshTopicsTable()"><i class="glyphicon glyphicon-refresh"></i>&nbsp;Refresh</button>';
             var tableToolBar = 'div.topics-table-toolbar';
             $(tableToolBar).html(refreshButtonHtml);
@@ -321,9 +321,7 @@
             };
             $log.info('Posts table definition', tableDefinition);
             $('table#postsTable').dataTable(tableDefinition);
-            var refreshButtonHtml = '<button ng-click="openModal(\'New Post\', \'' + ($scope.selectedTopic == undefined ? '' : $scope.selectedTopic.id) +
-                '\', \'\', \'' + ($scope.selectedTopic == undefined ? '' : $scope.selectedTopic.subject) +
-                '\', \'\')" class="btn btn-danger" type="button"><i class="glyphicon glyphicon-pencil"></i>&nbsp;New Post</button>' +
+            var refreshButtonHtml = '<button ng-click="openModal(\'New Post\', \'\', \'\')" class="btn btn-danger" type="button"><i class="glyphicon glyphicon-pencil"></i>&nbsp;New Post</button>' +
                 '<button class="btn btn-info" type="button" ng-click="refreshPostsTable()"><i class="glyphicon glyphicon-refresh"></i>&nbsp;Refresh</button>';
             var tableToolBar = 'div.posts-table-toolbar';
             $(tableToolBar).html(refreshButtonHtml);
@@ -442,7 +440,7 @@
             }
         });
 
-        $scope.openModal = function (modalTitle, topicId, postId, subject, text) {
+        $scope.openModal = function (modalTitle, postId, text) {
             var modalInstance = $uibModal.open({
                 animation: false,
                 templateUrl: 'modalContent.html',
@@ -453,13 +451,13 @@
                         return modalTitle;
                     },
                     topicId: function () {
-                        return topicId;
+                        return $scope.selectedTopic == undefined ? '' : $scope.selectedTopic.id;
                     },
                     postId: function () {
                         return postId;
                     },
                     subject: function () {
-                        return subject;
+                        return $scope.selectedTopic == undefined ? '' : $scope.selectedTopic.subject;
                     },
                     text: function () {
                         return text;
